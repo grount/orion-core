9aadce85a69937eb5e3a0dec33b50b42b8909aa9
Daniel Belenky <dbelenky@redhat.com>
2019-08-19 00:22:26 +0300

    Introduce gmail OAuth2 provider
    Implementation of the OAuth2 authentication flow for mobile
    and desktop apps:
    https://developers.google.com/identity/protocols/OAuth2InstalledApp
    
    Signed-off-by: Daniel Belenky <daniel.belenky@gmail.com>

---
+++ internal/providers/gmail/gmail_test.go
@@ -0,0 +1,47 @@
+package gmail
+
+import (
+	"context"
+	"testing"
+
+	"net/http"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMakeChallenge(t *testing.T) {
+	s := "somestring"
+	ret := makeChallenge(&s)
+	assert.Equal(t, ret, "Y_b-eXAm15Tg3D4r0nmu4Z3S-NtnSIFypkS7aHkqVww")
+}
+
+// Ensure that code is being sent on the channel
+func TestCreateResponseListenerDataIsSent(t *testing.T) {
+	srv, c := createResponseListener()
+	defer srv.Shutdown(context.TODO())
+	_, err := http.Get("http://" + srv.Addr + oauth2Endpoint + "?code=1234")
+	if err != nil {
+		assert.Fail(t, "Get request failed: "+err.Error())
+	}
+	select {
+	case r := <-*c:
+		assert.Equal(t, r, "1234")
+	case <-time.After(time.Second * 10):
+		assert.Fail(t, "Timed out waiting for code")
+	}
+}
+
+// Ensure that the channel is being closed after the server shuts down
+func TestCreateResponseListenerChannelIsClosed(t *testing.T) {
+	srv, c := createResponseListener()
+	srv.Shutdown(context.TODO())
+	select {
+	case d, ok := <-*c:
+		if ok {
+			assert.Fail(t, "Channel is still open and had data in the queue: "+d)
+		}
+	case <-time.After(time.Second * 10):
+		assert.Fail(t, "Channel is still open!")
+	}
+}
